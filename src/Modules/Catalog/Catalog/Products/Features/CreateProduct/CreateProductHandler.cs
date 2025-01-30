bdae9b09a6975b256b2f7174c7e4b88a9ecb92aa
Eduardo <eduardoivanuhgamino2@gmail.com>
2025-01-29 20:54:25 -0600

    Added features folder and CreateProductHandler

---
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Catalog.Products.Features.CreateProduct
+{
+    public record CreateProductCommand(
+        string Name,
+        ICollection<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price) : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
