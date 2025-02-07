9f7140f705c9cca9e072227b08b8c5a753aed02b
Eduardo <eduardoivanuhgamino2@gmail.com>
2025-02-06 21:01:59 -0600

    Added command to delete product

---
+++ src/Modules/Catalog/Catalog/Products/Features/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.Products.Features.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+    public record DeleteProductResult(bool IsSuccess);
+
+    public class DeleteProductHandler(CatalogDbContext catalogDbContext) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        private readonly CatalogDbContext _catalogDbContext = catalogDbContext;
+
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _catalogDbContext.Products.FindAsync([request.Id], cancellationToken);
+
+            if (product is null) throw new Exception($"Product with the key [{request.Id}] not found");
+
+            _catalogDbContext.Products.Remove(product);
+
+            await _catalogDbContext.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+        }
+    }
+}
