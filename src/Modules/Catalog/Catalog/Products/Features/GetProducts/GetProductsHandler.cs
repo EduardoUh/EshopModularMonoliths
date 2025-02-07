3f3e393199c25e379c228f6666684b1c20261f7b
Eduardo <eduardoivanuhgamino2@gmail.com>
2025-02-06 21:03:01 -0600

    Added query to get products

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.Products.Features.GetProducts
+{
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+
+    public record GetProductsResult(IEnumerable<ProductDTO> Products);
+
+    public class GetProductsHandler(CatalogDbContext catalogDbContext) : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        private readonly CatalogDbContext _catalogDbContext = catalogDbContext;
+
+        public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _catalogDbContext.Products
+                .AsNoTracking()
+                .OrderBy(product => product.Name)
+                .ToListAsync(cancellationToken);
+
+            var productsDTOs = products.Adapt<IEnumerable<ProductDTO>>();
+
+            return new GetProductsResult(productsDTOs);
+        }
+    }
+}
--- src/Modules/Catalog/Catalog/_Imports.cs
+++ src/Modules/Catalog/Catalog/_Imports.cs
@@ -3,6 +3,7 @@ global using Catalog.Data.Seed;
 global using Catalog.Products.DTOs;
 global using Catalog.Products.Events;
 global using Catalog.Products.Models;
+global using Mapster;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Diagnostics;
