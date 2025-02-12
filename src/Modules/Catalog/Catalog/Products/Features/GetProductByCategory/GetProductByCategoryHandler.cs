706107c0fbd89e3747ec66d098cde52204ef0d0c
Eduardo <eduardoivanuhgamino2@gmail.com>
2025-02-11 21:53:20 -0600

    Added handler to get products by category

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.Products.Features.GetProductByCategory
+{
+    public record class GetProductByCategoryQuery(string Category)
+        : IQuery<GetProductByCategoryResult>;
+
+    public record class GetProductByCategoryResult(IEnumerable<ProductDTO> Products);
+
+    public class GetProductByCategoryHandler(CatalogDbContext catalogDbContext) :
+        IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        private readonly CatalogDbContext _catalogDbContext = catalogDbContext;
+
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _catalogDbContext.Products
+                            .AsNoTracking()
+                            .Where(product => product.Category.Contains(request.Category))
+                            .OrderBy(product => product.Name)
+                            .ToListAsync(cancellationToken);
+
+            return new GetProductByCategoryResult(products.Adapt<IEnumerable<ProductDTO>>());
+        }
+    }
+}
