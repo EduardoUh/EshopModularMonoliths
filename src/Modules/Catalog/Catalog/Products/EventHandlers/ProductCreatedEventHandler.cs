e04c9c00ae91980f6558385f0e398d3b5860d27c
Eduardo <eduardoivanuhgamino2@gmail.com>
2025-02-11 21:55:53 -0600

    Added event handlers for product creation and product price update

---
+++ src/Modules/Catalog/Catalog/Products/EventHandlers/ProductCreatedEventHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Catalog.Products.EventHandlers
+{
+    public class ProductCreatedEventHandler(ILogger<ProductCreatedEventHandler> logger)
+        : INotificationHandler<ProductCreatedEvent>
+    {
+        private readonly ILogger<ProductCreatedEventHandler> _logger = logger;
+        public Task Handle(ProductCreatedEvent notification, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Domain event handled {DomainEvent}: Product {ProductName} with key [{Key}] was successfully created", notification.GetType().Name, notification.Product.Name, notification.Product.Id);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Modules/Catalog/Catalog/Products/EventHandlers/ProductPriceChangedEventHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.Products.EventHandlers
+{
+    public class ProductPriceChangedEventHandler(ILogger<ProductPriceChangedEventHandler> logger)
+        : INotificationHandler<ProductPriceChangedEvent>
+    {
+        private readonly ILogger<ProductPriceChangedEventHandler> _logger = logger;
+
+        public Task Handle(ProductPriceChangedEvent notification, CancellationToken cancellationToken)
+        {
+            // TODO: Publish product price changed integration event for updating basket prices
+
+            _logger.LogInformation("Domain event handled {DomainEvent}: Price of product {ProductName} successfully updated", notification.GetType().Name, notification.Product.Name);
+
+            return Task.CompletedTask;
+        }
+    }
+}
