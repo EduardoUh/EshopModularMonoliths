982013fe466e4c6d4f379c273782287cc7275433
Eduardo <eduardoivanuhgamino2@gmail.com>
2025-02-11 21:54:06 -0600

    Added handler to get product by id

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.Products.Features.GetProductById
+{
+    public record class GetProductByIdQuery(Guid Id)
+        : IQuery<GetProductByIdResult>;
+
+    public record class GetProductByIdResult(ProductDTO Product);
+
+    public class GetProductByIdHandler(CatalogDbContext catalogDbContext)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        private readonly CatalogDbContext _catalogDbContext = catalogDbContext;
+
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var product = await _catalogDbContext.Products
+                                .AsNoTracking()
+                                .FirstOrDefaultAsync(product => product.Id.Equals(request.Id), cancellationToken);
+
+            return product is null
+                ? throw new Exception($"Product with the key [{request.Id}] not found")
+                : new GetProductByIdResult(product.Adapt<ProductDTO>());
+        }
+    }
+}
