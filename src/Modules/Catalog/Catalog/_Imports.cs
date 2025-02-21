592ea7e092bae72a43b59b6d39e0d291cff78f62
Eduardo <eduardoivanuhgamino2@gmail.com>
2025-02-20 20:46:05 -0600

    Added endpoints for creating, updating and delete product

--- src/Modules/Catalog/Catalog/_Imports.cs
+++ src/Modules/Catalog/Catalog/_Imports.cs
@@ -1,4 +1,5 @@
-﻿global using Catalog.Data;
+﻿global using Carter;
+global using Catalog.Data;
 global using Catalog.Data.Seed;
 global using Catalog.Products.DTOs;
 global using Catalog.Products.Events;
@@ -6,6 +7,8 @@ global using Catalog.Products.Models;
 global using Mapster;
 global using MediatR;
 global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Routing;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Diagnostics;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
