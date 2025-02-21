82fe1b58fee22108d3f895e0ade058dfa5d76d7b
Eduardo <eduardoivanuhgamino2@gmail.com>
2025-02-20 21:49:58 -0600

    Added endpoints for getting product by id, by category and to get all products

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,26 @@
+﻿namespace Catalog.Products.Features.GetProductByCategory
+{
+    public record GetProductByCategoryResponse(IEnumerable<ProductDTO> Products);
+
+    public class GetProductByCategoryEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
+            {
+                var query = new GetProductByCategoryQuery(category);
+
+                var result = await sender.Send(query);
+
+                var response = result.Adapt<GetProductByCategoryResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetProductsByCategory")
+            .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products By Category")
+            .WithDescription("Get Products By Category");
+        }
+    }
+}
