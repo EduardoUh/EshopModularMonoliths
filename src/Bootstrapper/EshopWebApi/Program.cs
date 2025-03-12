23b0b6acbbf97352b01ba4423a8f34589f3b8b82
Eduardo <eduardoivanuhgamino2@gmail.com>
2025-03-11 22:01:45 -0600

    Implemented basic global exception handler

--- src/Bootstrapper/EshopWebApi/Program.cs
+++ src/Bootstrapper/EshopWebApi/Program.cs
@@ -24,4 +24,31 @@ app
     .UseCatalogModuleConfigurations()
     .UseOrderingModuleConfigurations();
 
+app.UseExceptionHandler(exceptionHandler =>
+{
+    exceptionHandler.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+        if (exception is null) return;
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+
+        logger.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        context.Response.ContentType = "application/problem+json";
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
+
 app.Run();
--- src/Bootstrapper/EshopWebApi/_Imports.cs
+++ src/Bootstrapper/EshopWebApi/_Imports.cs
@@ -1,5 +1,7 @@
 ﻿global using Basket;
 global using Carter;
 global using Catalog;
+global using Microsoft.AspNetCore.Diagnostics;
+global using Microsoft.AspNetCore.Mvc;
 global using Ordering;
 global using Shared.Extentions;
